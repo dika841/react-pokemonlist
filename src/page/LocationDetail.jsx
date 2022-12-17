91edc303e2d99fb5b156a6c9e59605cbcc5166e1
Randika <randikaa1999@gmail.com>
2022-12-17 20:40:29 +0700

    Update file LocationDetails.jsx

--- src/page/LocationDetail.jsx
+++ src/page/LocationDetail.jsx
@@ -14,40 +14,42 @@ const LocationDetail = () => {
   const { id } = useParams();
   const url = "https://pokeapi.co/api/v2/location-area";
 
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      setLoading(true);
-      const response = await fetch(url + `/${id}`);
-      const result = await response.json();
-
-      const fetchLocation = async (url) => {
-        const responseLocation = await fetch(url);
-        const resultLocation = await responseLocation.json();
-        return resultLocation;
-      };
-
-      const urls = await result.pokemon_encounters.map((el) => el.pokemon.url);
-      const fetchArray = urls.map(fetchLocation);
-      const pokemonLocation = await Promise.all(fetchArray);
-      const pokemons = pokemonLocation.map((el) => ({
-        name: el.name,
-        imgUrl: el.sprites.other.dream_world.front_default,
-      }));
-
-      setData(pokemons);
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
-    setLoading(false);
-  };
-
   console.log(data);
 
   useEffect(() => {
-    fetchData(name, img);
-  }, [name, img]);
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        setLoading(true);
+        const response = await fetch(url + `/${id}`);
+        const result = await response.json();
+
+        const fetchLocation = async (url) => {
+          const responseLocation = await fetch(url);
+          const resultLocation = await responseLocation.json();
+          return resultLocation;
+        };
+
+        const urls = await result.pokemon_encounters.map(
+          (el) => el.pokemon.url
+        );
+        const fetchArray = urls.map(fetchLocation);
+        const pokemonLocation = await Promise.all(fetchArray);
+        const pokemons = pokemonLocation.map((el) => ({
+          name: el.name,
+          imgUrl: el.sprites.other.dream_world.front_default,
+        }));
+
+        setData(pokemons);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [id]);
 
   const changes = (name, img) => {
     setName(name);
